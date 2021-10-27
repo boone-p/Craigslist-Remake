7cb452fbfcebbdf7e4184b2612d32ebcecc73ddf
ryangregerson14 <71282507+ryangregerson14@users.noreply.github.com>
2021-10-27 16:34:48 -0700

    created add, delete, and get database functions

--- database/database.js
+++ database/database.js
@@ -1,14 +1,44 @@
 // Database for Free Stuff App
-// like user-services.js, this will do database operations and maybe call SQL queries
+// This file will do database operations
 
-const mongoose = require('mongoose');
-// const productModel = require("./product");
-// const sellerModel = require("./user");
+const mongoose = require("mongoose");
+const productModel = require("./productSchema");
+// const userModel = require("./userSchema");
 
-mongoose.connect(
-    'mongodb://localhost:27017/FreeStuff',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-).catch(error => console.log(error));
+mongoose
+	.connect("mongodb://localhost:27017/FreeStuffApp", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.catch((error) => console.log(error));
+
+async function getProducts() {
+	let result = await productModel.find();
+	return result;
+}
+
+async function getUsers() {}
+
+async function addProduct(product) {
+	try {
+		const prodToAdd = new productModel(product);
+		const savedProd = await prodToAdd.save();
+		return savedProd;
+	} catch (error) {
+		console.log(error);
+		return false;
+	}
+}
+
+async function addUser(user) {}
+
+async function deleteProduct(id) {
+	try {
+		return await productModel.findByIdAndDelete(id);
+	} catch (error) {
+		console.log(error);
+		return undefined;
+	}
+}
+
+async function deleteUser(id) {}
