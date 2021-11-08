201d6ac5c6b6616f592a61bac0f47e111466b01c
boone-p <boone.purnell@gmail.com>
2021-11-08 15:08:47 -0800

    about page

---
+++ frontend/src/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const head = {
+    margin: '40px',
+
+
+}
+
+function About() {
+    return (
+        <h2 style={head}>This is a project made by Cal Poly students in Dr Bruno da Silva's Intro to Software Engineering class!</h2>
+    );
+}
+    
+
+export default About;
